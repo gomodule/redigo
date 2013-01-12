c047d468c974777dd006401a9046cb5842aead47
Matthew Baird <mattbaird@gmail.com>
2013-01-12 15:00:37 -0800

    Add new functionality to TestOnBorrow from the pool. If you define a TestOnBorrow function, it will be executed before your newly requested redid connection is returned. Will retry for all connections in the pool

--- redis/pool.go
+++ redis/pool.go
@@ -51,6 +51,13 @@ var errPoolClosed = errors.New("redigo: connection pool closed")
 //                  }
 //                  return c, err
 //              },
+// 				//custom connection test method
+//				TestOnBorrow: func(c redis.Conn) error {
+//				if _, err := c.Do("PING"); err != nil {
+//					return err
+//				}
+//				return nil
+//			},
 //          }
 //
 // This pool has a maximum of three connections to the server specified by the
@@ -66,6 +73,9 @@ type Pool struct {
 
 	// Dial is an application supplied function for creating new connections.
 	Dial func() (Conn, error)
+
+	// Dial is an application supplied function for creating new connections.
+	TestOnBorrow func(Conn) error
 
 	// Maximum number of idle connections in the pool.
 	MaxIdle int
@@ -134,10 +144,30 @@ func (p *Pool) get() (c Conn, err error) {
 		p.mu.Unlock()
 		return nil, errors.New("redigo: get on closed pool")
 	}
-	if e := p.idle.Front(); e != nil {
-		c = e.Value.(idleConn).c
-		p.idle.Remove(e)
+	// get a new connection. If the pool has a TestOnBorrow function defined,
+	// execute the function to test the connection.
+	for {
+		e := p.idle.Front()
+		if e != nil {
+			c = e.Value.(idleConn).c
+			p.idle.Remove(e)
+			if p.TestOnBorrow != nil {
+				err := p.TestOnBorrow(c)
+				if err != nil {
+					p.mu.Unlock()
+					c.Close()
+					p.mu.Lock()
+				} else {
+					break
+				}
+			} else {
+				break
+			}
+		} else {
+			break
+		}
 	}
+
 	p.mu.Unlock()
 	if c == nil {
 		c, err = p.Dial()
