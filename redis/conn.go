34750db78c614415292b5190fb50aa7abbcddf81
Steven Hartland <steven.hartland@multiplay.co.uk>
2016-03-11 11:21:36 +0000

    Add custom argument representation handling
    Add the ability for types to control how they are sent when used as
    arguments to redis commands.
    
    This is useful for types which don't want to use their Stringer
    representation when used as an argument to a redis command.
    
    Examples of this is a time.Duration which the user wants to store
    the number of seconds or a time.Time being stored as int instead of
    a string.

--- redis/conn.go
+++ redis/conn.go
@@ -370,6 +370,10 @@ func (c *conn) writeCommand(cmd string, args []interface{}) (err error) {
 			}
 		case nil:
 			err = c.writeString("")
+		case Argument:
+			var buf bytes.Buffer
+			fmt.Fprint(&buf, arg.RedisArg())
+			err = c.writeBytes(buf.Bytes())
 		default:
 			var buf bytes.Buffer
 			fmt.Fprint(&buf, arg)
