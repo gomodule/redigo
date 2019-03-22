a5f11d43e42331210ed58fcc81c1cf38451f9b17
izumin5210 <m@izum.in>
2019-03-22 14:11:19 +0900

    Add description about Dial and DialContext

--- redis/pool.go
+++ redis/pool.go
@@ -57,6 +57,7 @@ var (
 //    return &redis.Pool{
 //      MaxIdle: 3,
 //      IdleTimeout: 240 * time.Second,
+//      // Dial or DialContext must be set. When both are set, DialContext takes precedence over Dial.
 //      Dial: func () (redis.Conn, error) { return redis.Dial("tcp", addr) },
 //    }
 //  }
