gomodule/redigo

commit 4f569a4708584d192c9b2e49bb2d9936341f1e68

feat: Add ptr support for scan and flatten
Add support to scan to a ptr and flatten from a ptr.

redis/scan.go
@@ -99,6 +99,8 @@ func convertAssignString(d reflect.Value, s string) (err error) {
 		} else {
 			err = cannotConvert(d, s)
 		}
+	case reflect.Ptr:
+		err = convertAssignString(d.Elem(), s)
 	default:
 		err = cannotConvert(d, s)
 	}
@@ -644,7 +646,13 @@ func flattenStruct(args Args, v reflect.Value) Args {
 				continue
 			}
 		}
-		args = append(args, fs.name, fv.Interface())
+		if fv.Kind() == reflect.Ptr {
+			if !fv.IsNil() {
+				args = append(args, fs.name, fv.Elem().Interface())
+			}
+		} else {
+			args = append(args, fs.name, fv.Interface())
+		}
 	}
 	return args
 }

redis/scan_test.go
@@ -201,18 +201,21 @@ type s0 struct {
 }
 
 type s1 struct {
-	X  int    `redis:"-"`
-	I  int    `redis:"i"`
-	U  uint   `redis:"u"`
-	S  string `redis:"s"`
-	P  []byte `redis:"p"`
-	B  bool   `redis:"b"`
-	Bt bool
-	Bf bool
+	X    int    `redis:"-"`
+	I    int    `redis:"i"`
+	U    uint   `redis:"u"`
+	S    string `redis:"s"`
+	P    []byte `redis:"p"`
+	B    bool   `redis:"b"`
+	Bt   bool
+	Bf   bool
+	PtrB *bool
 	s0
 	Sd  durationScan  `redis:"sd"`
 	Sdp *durationScan `redis:"sdp"`
 }
+
+var boolTrue = true
 
 var scanStructTests = []struct {
 	title string
@@ -228,22 +231,24 @@ var scanStructTests = []struct {
 			"b", "t",
 			"Bt", "1",
 			"Bf", "0",
+			"PtrB", "1",
 			"X", "123",
 			"y", "456",
 			"sd", "1m",
 			"sdp", "1m",
 		},
 		&s1{
-			I:   -1234,
-			U:   5678,
-			S:   "hello",
-			P:   []byte("world"),
-			B:   true,
-			Bt:  true,
-			Bf:  false,
-			s0:  s0{X: 123, Y: 456},
-			Sd:  durationScan{Duration: time.Minute},
-			Sdp: &durationScan{Duration: time.Minute},
+			I:    -1234,
+			U:    5678,
+			S:    "hello",
+			P:    []byte("world"),
+			B:    true,
+			Bt:   true,
+			Bf:   false,
+			PtrB: &boolTrue,
+			s0:   s0{X: 123, Y: 456},
+			Sd:   durationScan{Duration: time.Minute},
+			Sdp:  &durationScan{Duration: time.Minute},
 		},
 	},
 	{"absent values",
@@ -421,17 +426,19 @@ var argsTests = []struct {
 }{
 	{"struct ptr",
 		redis.Args{}.AddFlat(&struct {
-			I  int               `redis:"i"`
-			U  uint              `redis:"u"`
-			S  string            `redis:"s"`
-			P  []byte            `redis:"p"`
-			M  map[string]string `redis:"m"`
-			Bt bool
-			Bf bool
+			I    int               `redis:"i"`
+			U    uint              `redis:"u"`
+			S    string            `redis:"s"`
+			P    []byte            `redis:"p"`
+			M    map[string]string `redis:"m"`
+			Bt   bool
+			Bf   bool
+			PtrB *bool
+			PtrI *int
 		}{
-			-1234, 5678, "hello", []byte("world"), map[string]string{"hello": "world"}, true, false,
+			-1234, 5678, "hello", []byte("world"), map[string]string{"hello": "world"}, true, false, &boolTrue, nil,
 		}),
-		redis.Args{"i", int(-1234), "u", uint(5678), "s", "hello", "p", []byte("world"), "m", map[string]string{"hello": "world"}, "Bt", true, "Bf", false},
+		redis.Args{"i", int(-1234), "u", uint(5678), "s", "hello", "p", []byte("world"), "m", map[string]string{"hello": "world"}, "Bt", true, "Bf", false, "PtrB", true},
 	},
 	{"struct",
 		redis.Args{}.AddFlat(struct{ I int }{123}),

commit b36e4737e27c6a83a57c72338fcd19a6d026cf2c

fix: Argument processing on pointers
Fix Argument interface processing on struct pointer types which was
broken by #428 and not quite corrected by #446.

go.sum
@@ -7,4 +7,5 @@ github.com/stretchr/testify v1.5.1 h1:nOGnQDM7FYENwehXlg/kFVnos3rEvtKTjRvOWSzb6H
 github.com/stretchr/testify v1.5.1/go.mod h1:5W2xD1RspED5o8YsWQXVCued0rvSQ+mT+I5cxcmMvtA=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 h1:yhCVgyC4o1eVCa2tZl7eS0r+SDo693bJlVdllGtEeKM=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405/go.mod h1:Co6ibVJAznAaIkqp8huTwlJQCZ016jof/cbN4VW5Yz0=
+gopkg.in/yaml.v2 v2.2.2 h1:ZCJp+EgiOT7lHqUV2J862kp8Qj64Jo6az82+3Td9dZw=
 gopkg.in/yaml.v2 v2.2.2/go.mod h1:hI93XBmqTisBFMUTm0b8Fm+jr3Dg1NNxqwp+5A1VGuI=

redis/scan.go
@@ -646,16 +646,14 @@ func flattenStruct(args Args, v reflect.Value) Args {
 				continue
 			}
 		}
-		if fv.Kind() == reflect.Ptr {
+		if arg, ok := fv.Interface().(Argument); ok {
+			args = append(args, fs.name, arg.RedisArg())
+		} else if fv.Kind() == reflect.Ptr {
 			if !fv.IsNil() {
 				args = append(args, fs.name, fv.Elem().Interface())
 			}
 		} else {
-			if arg, ok := fv.Interface().(Argument); ok {
-				args = append(args, fs.name, arg.RedisArg())
-			} else {
-				args = append(args, fs.name, fv.Interface())
-			}
+			args = append(args, fs.name, fv.Interface())
 		}
 	}
 	return args

redis/scan_test.go
@@ -419,12 +419,14 @@ func ExampleScanSlice() {
 	// [{Earthbound 1} {Beat 4} {Red 5}]
 }
 
+var now = time.Now()
+
 var argsTests = []struct {
 	title    string
 	actual   redis.Args
 	expected redis.Args
 }{
-	{"struct ptr",
+	{"struct-ptr",
 		redis.Args{}.AddFlat(&struct {
 			I    int               `redis:"i"`
 			U    uint              `redis:"u"`
@@ -444,15 +446,23 @@ var argsTests = []struct {
 		redis.Args{}.AddFlat(struct{ I int }{123}),
 		redis.Args{"I", 123},
 	},
-	{"struct with RedisArg",
+	{"struct-with-RedisArg-direct",
 		redis.Args{}.AddFlat(struct{ T CustomTime }{CustomTime{Time: time.Unix(1573231058, 0)}}),
+		redis.Args{"T", int64(1573231058)},
+	},
+	{"struct-with-RedisArg-direct-ptr",
+		redis.Args{}.AddFlat(struct{ T *CustomTime }{&CustomTime{Time: time.Unix(1573231058, 0)}}),
+		redis.Args{"T", int64(1573231058)},
+	},
+	{"struct-with-RedisArg-ptr",
+		redis.Args{}.AddFlat(struct{ T *CustomTimePtr }{&CustomTimePtr{Time: time.Unix(1573231058, 0)}}),
 		redis.Args{"T", int64(1573231058)},
 	},
 	{"slice",
 		redis.Args{}.Add(1).AddFlat([]string{"a", "b", "c"}).Add(2),
 		redis.Args{1, "a", "b", "c", 2},
 	},
-	{"struct omitempty",
+	{"struct-omitempty",
 		redis.Args{}.AddFlat(&struct {
 			Sdp *durationArg `redis:"Sdp,omitempty"`
 		}{
@@ -464,10 +474,20 @@ var argsTests = []struct {
 
 func TestArgs(t *testing.T) {
 	for _, tt := range argsTests {
-		if !reflect.DeepEqual(tt.actual, tt.expected) {
-			t.Fatalf("%s is %v, want %v", tt.title, tt.actual, tt.expected)
-		}
+		t.Run(tt.title, func(t *testing.T) {
+			if !reflect.DeepEqual(tt.actual, tt.expected) {
+				t.Fatalf("is %v, want %v", tt.actual, tt.expected)
+			}
+		})
 	}
+}
+
+type CustomTimePtr struct {
+	time.Time
+}
+
+func (t *CustomTimePtr) RedisArg() interface{} {
+	return t.Unix()
 }
 
 type CustomTime struct {